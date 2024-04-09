ae20b4f23b89388041a874688764e8f712101b14
SaJid961 <sajidhussain98@gmail.com>
2024-04-10 01:33:29 +0530

    login  component

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,6 +3,7 @@ import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Container from "./container/Container";
+import Button from "./Button";
+import Input from "./Input";
 
-
-export { Header, Footer, Container, Logo,LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
